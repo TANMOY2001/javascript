aa06aa5ac6f3199c2146320137bf5a886f14ae83
TANMOY2001 <tanmoyghosh1018@gmail.com>
2025-04-06 03:20:13 +0530

    string_intrpo

---
+++ Besics_js/string.js
@@ -0,0 +1,9 @@
+//Back tick Syntax
+let text = `hii my name is "tanmoy"`
+// console.log(text)
+// console.log(typeof(name))
+//String interpolation
+const name = "tanmoy"
+const age = 20  
+console.log(`my name is ${name} and my age is "${age}"`)
+//String methods
