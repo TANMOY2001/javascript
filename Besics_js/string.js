436e181698f40f5542ba2d5c508f68d6b0a6a03f
TANMOY2001 <tanmoyghosh1018@gmail.com>
2025-04-07 02:39:47 +0530

    07Apr25UPDATE

--- Besics_js/string.js
+++ Besics_js/string.js
@@ -1,9 +1,16 @@
 //Back tick Syntax
-let text = `hii my name is "tanmoy"`
+// let text = `hii my name is "tanmoy"`
 // console.log(text)
 // console.log(typeof(name))
-//String interpolation
-const name = "tanmoy"
-const age = 20  
-console.log(`my name is ${name} and my age is "${age}"`)
-//String methods
+// --String interpolation
+// const name = "tanmoy"
+// const age = 20  
+// console.log(`my name is ${name} and my age is "${age}"`)
+// --string object
+// const str = new String("Tanmoy")
+// console.log(str[0])
+// console.log(str)
+// console.log(str.length)
+// --const str = "Tanmoy"
+// console.log(str[0])
+console.log(str.indexOf('a'))
---
+++ Besics_js/var.js
@@ -0,0 +1,19 @@
+// // console.log(x);
+// const x = 5;
+// console.log (x);
+// x = 7;
+// console.log(x);
+// a = x + x;
+// console.log (a);
+// let x = 5;
+// x+=10;
+// console.log(x);
+// let num = 5;
+// console.log(num);
+let num1=new Number(100);
+// console.log(num1[2,3]);
+// console.log(typeof(num1));
+// console.log(typeof (num));
+
+console.log(num1.toString());
+console.log(typeof(num1)); 
